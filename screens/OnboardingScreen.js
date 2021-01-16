43572b702c2d6a8ebf5eed271b05367aece51235
T3sultan <incognito.soul011@gmail.com>
2021-01-16 23:12:23 +0600

    OnboardingScreen and LoginScreen

---
+++ screens/OnboardingScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View, Text,Button, StyleSheet} from 'react-native';
+
+const OnboardingScreen = ({navigation}) =>{
+    return (
+        <View style={styles.container}>
+           <Text>Onboarding Screen</Text>
+            <Button
+                title='Click Here'
+                onPress={()=> navigation.navigate('Login')}
+            />
+        </View>
+    );
+};
+export default OnboardingScreen;
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+})

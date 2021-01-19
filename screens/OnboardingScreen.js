341ec60768b3dde615a6fd532e22c78f2a3fcb62
T3sultan <incognito.soul011@gmail.com>
2021-01-19 22:59:25 +0600

    image swaper

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,15 +1,69 @@
 import React from 'react';
-import {View, Text,Button, StyleSheet} from 'react-native';
+import {View, Text,Button,Image, StyleSheet} from 'react-native';
+
+import Onboarding from 'react-native-onboarding-swiper';
 
 const OnboardingScreen = ({navigation}) =>{
     return (
-        <View style={styles.container}>
-           <Text>Onboarding Screen</Text>
-            <Button
-                title='Click Here'
-                onPress={()=> navigation.navigate('Login')}
-            />
-        </View>
+        <Onboarding
+         onSkip={()=>navigation.navigate('Login')}
+         onDone={()=>navigation.navigate('Login')}
+        pages={[
+          {
+            backgroundColor: '#98da11',
+            image: <Image source={require('../assets/logo.png')} 
+            style={{height:150,width:300}}/>,
+            title: 'React Native Color',
+            subtitle: 'Done with React Native Onboarding Swiper',
+          },
+          
+          {
+            backgroundColor: '#a6e4d0',
+            image: <Image source={require('../assets/undraw_Destination_re_sr74.png')} 
+            style={{height:300,width:400}}/>,
+            title: 'Searching Map',
+            subtitle: 'Selected location Bangladesh',
+          },
+          {
+            backgroundColor: '#fdeb90',
+            image: <Image source={require('../assets/undraw_Confirmation_re_b6q5.png')} 
+            style={{height:300,width:400}}/>,
+            title: 'Text Box',
+            subtitle: 'Friends conversation all time',
+          },
+          {
+            backgroundColor: '#87ceeb',
+            image: <Image source={require('../assets/undraw_my_app_re_gxtj.png')} 
+            style={{height:300,width:400}}/>,
+            title: 'Online Platfrom',
+            subtitle: 'Twitter news update',
+          },
+          {
+            backgroundColor: '#ee82ee',
+            image: <Image source={require('../assets/onboarding-img3.png')} 
+            style={{height:300,width:400}}/>,
+            title: 'Shopping Card',
+            subtitle: 'Shopping card react native every person',
+          },
+          {
+            backgroundColor: '#ffdab9',
+            image: <Image source={require('../assets/undraw_Selection_re_poer.png')} 
+            style={{height:300,width:400}}/>,
+            title: 'Notification ',
+            subtitle: 'Media notification alert for Human ',
+          },
+          {
+            backgroundColor: '#98fb98',
+            image: <Image source={require('../assets/undraw_subscribe_vspl.png')} 
+            style={{height:300,width:400}}/>,
+            title: 'Mail Box',
+            subtitle: 'Gmail selected for new person that candidat',
+          },
+         
+         
+         
+        ]}
+      />
     );
 };
 export default OnboardingScreen;
